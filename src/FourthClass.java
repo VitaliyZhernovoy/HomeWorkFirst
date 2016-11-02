1ec6a13c73989d03d28dfb204e1ce385dfcafb53
USER <vitaliyzhernovoy@gmail.com>
2016-11-02 16:01:21 +0200

    First HomeWork

---
+++ src/FourthClass.java
@@ -0,0 +1,7 @@
+
+public class FourthClass {
+    public static void main(String[] args) {
+        System.out.println(); // с отступом вроде лучше
+        System.out.println("Мой четвертый класс на Java");
+    }
+}
